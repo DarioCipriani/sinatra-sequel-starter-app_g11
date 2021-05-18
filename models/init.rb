e6a7d7dd41f3c11cb15e6d5e4c636df19c7caa1c
DarioCipriani <dario-cipriani@hotmail.com>
2021-05-17 22:21:03 -0300

    registrando modelos

--- models/init.rb
+++ models/init.rb
@@ -1,1 +1,7 @@
 require_relative 'post'
+require_relative 'career'
+require_relative 'question'
+require_relative 'survey'
+require_relative 'choice'
+require_relative 'response'
+require_relative 'outcome'
