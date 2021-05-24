b934223e46087638e35a936c2ce6ad8edbea3a00
DarioCipriani <dario-cipriani@hotmail.com>
2021-05-24 20:31:45 -0300

    creamos las vistas landing y careers y mejoramos el app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require './models/init.rb'
 
 class App < Sinatra::Base
   get '/' do
-    "Hello World"
+    erb :landing
   end
 
   get "/hello/:name" do
@@ -10,9 +10,9 @@ class App < Sinatra::Base
    erb :hello_template
   end
 
+  #este es el post para crear carrera
   post "/careers" do
-    data = JSON.parse request.body.read
-    career = Career.new(name: data['name'])
+    career = Career.new(name: params[:name])
 
     if career.save
       [201, { 'location' => "careers/#{career.id}" }, 'CREATED']
@@ -21,12 +21,52 @@ class App < Sinatra::Base
     end
   end
 
+  #este es el get para mostrar las carreras
+  get "/careers" do
+    @careers = Career.all
+
+    erb :careers_index
+  end
+
+  #este es el get para mostrar las carreras con la descripciond de cada una
+  get "/careers/:id" do
+    career = Career.where(id: params['id']).last
+    "<h1> Resumen de la carrera #{career.name}</h1>" +
+    "<ul>" +
+    "<li> id: #{career.id}" +
+    "<li> name: #{career.name}" +
+    "<li> surveys count: #{career.surveys.count}" +
+    "</ul>"
+  end
+
   post "/surveys" do
     data = JSON.parse request.body.read
-    survey = Survey.new(username: data['username'])
+    survey = Survey.new(username: data['username'], career_id: data['career_id'])
 
     if survey.save
       [201, { 'location' => "surveys/#{survey.id}" }, 'CREATED']
+    else
+      [500, {}, 'Internal Server Error']
+    end
+  end
+
+  post "/responses" do
+    data = JSON.parse request.body.read
+    response = Response.new(question_id: data['question_id'], choise_id: data['choise_id'], survey_id: data['survey_id'])
+
+    if response.save
+      [201, { 'location' => "responses/#{response.id}" }, 'CREATED']
+    else
+      [500, {}, 'Internal Server Error']
+    end
+  end
+
+  post "/outcomes" do
+    data = JSON.parse request.body.read
+    outcome = Outcome.new(career_id: data['career_id'], choise_id: data['choise_id'])
+
+    if outcome.save
+      [201, { 'location' => "outcomes/#{outcome.id}" }, 'CREATED']
     else
       [500, {}, 'Internal Server Error']
     end
@@ -45,7 +85,7 @@ class App < Sinatra::Base
 
   post "/choises" do
     data = JSON.parse request.body.read
-    choise = Choise.new(text: data['text'])
+    choise = Choise.new(text: data['text'], question_id: data['question_id'])
 
     if choise.save
       [201, { 'location' => "choises/#{choise.id}" }, 'CREATED']
