c699d09224d89508fd22d68186a8a3f33758bf4c
DarioCipriani <dario-cipriani@hotmail.com>
2021-06-24 21:33:39 -0300

    limpiando codigo

--- app.rb
+++ app.rb
@@ -5,11 +5,6 @@ class App < Sinatra::Base
     erb :landing
   end
 
-  get "/hello/:name" do
-   @name = params[:name]
-   erb :hello_template
-  end
-
   #este es el post para crear carrera
   post "/careers" do
     career = Career.new(name: params[:name])
@@ -43,6 +38,8 @@ class App < Sinatra::Base
     redirect '/surveys'
   end
 
+  # cuando las preguntas fueron todas respondidas, el html
+  # llama a este post para calcular el resultado final de la encuesta
   post "/finish_survey" do
     data = JSON.parse request.body.read
     # obtengo valor del usuario
@@ -89,12 +86,12 @@ class App < Sinatra::Base
      redirect to "/finish/#{@survey.id}"
   end
 
+ # con el id del survey ganador, obtenemos la carrera y con estos datos
+ # llamamos a la ultima pagina con los datos ganadores
  get '/finish/:id' do
     @survey = Survey.find(id: params[:id])
-
-   @career = Career.find(id: @survey.career_id)
-
-   erb :finish
+    @career = Career.find(id: @survey.career_id)
+    erb :finish
 end
 
     def createResponses(data, survey_id)
@@ -103,33 +100,10 @@ end
       end
     end
 
-      #este es el get para mostrar las surveys
+  #este es el get para mostrar las surveys
   get "/surveys" do
     @questions = Question.all
     erb :surveys_index
-  end
-
-
-  post "/responses" do
-    data = JSON.parse request.body.read
-    response = Response.new(question_id: data['question_id'], choise_id: data['choise_id'], survey_id: data['survey_id'])
-
-    if response.save
-      [201, { 'location' => "responses/#{response.id}" }, 'CREATED']
-    else
-      [500, {}, 'Internal Server Error']
-    end
-  end
-
-  post "/outcomes" do
-    data = JSON.parse request.body.read
-    outcome = Outcome.new(career_id: data['career_id'], choise_id: data['choise_id'])
-
-    if outcome.save
-      [201, { 'location' => "outcomes/#{outcome.id}" }, 'CREATED']
-    else
-      [500, {}, 'Internal Server Error']
-    end
   end
 
    #este es el post para crear las questions
@@ -140,7 +114,7 @@ end
     redirect '/questions'
   end
 
-    #este es el get para mostrar las questions
+  #este es el get para mostrar las questions
   get "/questions" do
     @questions = Question.all
 
@@ -159,35 +133,5 @@ end
     "</ul>"
   end
 
-
-
-
-
-  post "/choises" do
-    data = JSON.parse request.body.read
-    choise = Choise.new(text: data['text'], question_id: data['question_id'])
-
-    if choise.save
-      [201, { 'location' => "choises/#{choise.id}" }, 'CREATED']
-    else
-      [500, {}, 'Internal Server Error']
-    end
-  end
-
-  #post "/posts" do
-    #request.body.rewind  # in case someone already read it
-    #data = JSON.parse request.body.read
-    #post = Post.new(description: data['desc'])
-    #if post.save
-      #[201, { 'Location' => "posts/#{post.id}" }, 'CREATED']
-    #else
-      #[500, {}, 'Internal Server Error']
-    #end
-  #end
-#
-  #get '/posts' do
-    #p = Post.where(id: 1).last
-    #p.description
-  #end
 end
 
